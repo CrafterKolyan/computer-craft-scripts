13950167127ad8f92a89434b991e8d25c7a47d12
Nikolai Korolev <korolevns98@gmail.com>
2023-11-05 14:08:43 +0000

    Dump table

--- barrel-opener.lua
+++ barrel-opener.lua
@@ -1,6 +1,21 @@
+function dump(o)
+   if type(o) == 'table' then
+      local s = '{ '
+      for k,v in pairs(o) do
+         if type(k) ~= 'number' then k = '"'..k..'"' end
+         s = s .. '['..k..'] = ' .. dump(v) .. ','
+      end
+      return s .. '} '
+   else
+      return tostring(o)
+   end
+end
+
 function step()
-    if turtle.detect() then
-        turtle.suck()
+    turtle.dig()
+    for i = 1, 16 do
+        local data = turtle.getItemDetail(16)
+        print(dump(data))
     end
 end
 
